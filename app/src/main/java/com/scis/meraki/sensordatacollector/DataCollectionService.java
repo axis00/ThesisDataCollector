9d7fec6c1d3092d06d9eebe1698296c63f2fa835
kharya0 <kkdvillegas@gmail.com>
2019-05-21 19:40:48 +0800

    plain data collector

--- app/src/main/java/com/scis/meraki/sensordatacollector/DataCollectionService.java
+++ app/src/main/java/com/scis/meraki/sensordatacollector/DataCollectionService.java
@@ -9,6 +9,7 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.nfc.Tag;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
@@ -37,6 +38,8 @@ import java.lang.Float;
  */
 public class DataCollectionService extends IntentService implements SensorEventListener{
 
+    public static final String TAG = "TAG";
+
     // TODO: Rename actions, choose action names that describe tasks that this
     // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
 
@@ -47,21 +50,14 @@ public class DataCollectionService extends IntentService implements SensorEventL
     private Interpreter tflite;
     String modelFile = "CNN_3.tflite";
 
-    // Debugging
-    private static final String TAG = "BluetoothService";
-
     //sensor related variables
     private SensorManager mSensorManager = null;
     private Sensor accelerometerSensor;
-    private Sensor gyroscopeSensor;
     private static int samplingRate = 20000;
 
     private static long timeInterval, prevTime, currTime;
-    private static String dataBuffer;
-    private static int tick = 0;
 
     private int accTick = 0;
-    private int gyroTick = 0;
 
     //flags
     private static boolean isGettingData = false;
@@ -70,78 +66,13 @@ public class DataCollectionService extends IntentService implements SensorEventL
 
     //inputData
     private static int windowSize = 100;
-    private static int dims = 6;
+    private static int dims = 3;
 
-    private static float[][] gyroData = new float[3][windowSize];
     private static float[][] accData = new float[3][windowSize];
 
     private static float[] inputData = new float[windowSize * dims];
 
     private static float[][] accSensorData = new float[3][windowSize];
-    private static float[][] gyroSensorData = new float[3][windowSize];
-
-    Queue<Float> bufferQueue = new LinkedList<>();
-
-    //bluetooth
-    private static BluetoothService btService;
-    @SuppressLint("HandlerLeak")
-    private final Handler btHandler = new Handler(){
-
-        @Override
-        public void handleMessage(Message msg) {
-            super.handleMessage(msg);
-            switch (msg.what){
-                case Constants.MESSAGE_TOAST:
-
-                    Toast.makeText(context, msg.getData().getString(Constants.TOAST),
-                            Toast.LENGTH_SHORT).show();
-
-                    break;
-
-                case Constants.MESSAGE_READ:
-
-                    byte[] data = new byte[msg.arg1];
-                    byte[] buffer = (byte[])msg.obj;
-                    for(int i = 0; i < msg.arg1; i++){
-                        data[i] = buffer[i];
-                    }
-
-                    String dataStr = new String(data, StandardCharsets.UTF_8);
-
-                    Log.d(TAG, "handleMessage: " + dataStr);
-
-                    interpretData(dataStr);
-                    
-                    break;
-            }
-        }
-
-        private void interpretData(String data){
-            if(data.charAt(0) == 'C'){
-                String[] command = data.split(" ");
-                String op = command[1];
-
-                switch(op){
-                    case  Constants.OP_SETFREQ:
-                        float freq = Float.parseFloat(command[2]);
-                        setFreq(freq);
-                        break;
-                }
-
-            }
-        }
-
-        private void setFreq(float freq){
-            samplingRate = (int)(1000000/freq);
-            stopSensors();
-            Log.d(TAG, "setFreq: restarting");
-            startSensor();
-            String res = "RES Frequency set to " + freq + "hz";
-            btService.write(res.getBytes(StandardCharsets.UTF_8));
-        }
-    };
-
-
 
     public DataCollectionService() {
         super("DataCollectionService");
@@ -156,11 +87,11 @@ public class DataCollectionService extends IntentService implements SensorEventL
         timeInterval = 1000;
 
         //create tflite object, loaded from model file
-        try {
-            tflite = new Interpreter(loadModelFile());
-        } catch (Exception ex){
-            ex.printStackTrace();
-        }
+//        try {
+//            tflite = new Interpreter(loadModelFile());
+//        } catch (Exception ex){
+//            ex.printStackTrace();
+//        }
 
     }
 
@@ -196,17 +127,11 @@ public class DataCollectionService extends IntentService implements SensorEventL
 
         startSensor();
 
-        if(btService == null){
-            btService = new BluetoothService(context,btHandler);
-            btService.start();
-        } else {
-            btService.start();
-        }
-
     }
     private void handleActionStop() {
+
         stopSensors();
-        btService.stop();
+
     }
 
     private void startSensor(){
@@ -215,9 +140,6 @@ public class DataCollectionService extends IntentService implements SensorEventL
             mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
             accelerometerSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
             mSensorManager.registerListener(this, accelerometerSensor, samplingRate);
-
-            gyroscopeSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
-            mSensorManager.registerListener(this, gyroscopeSensor, samplingRate);
 
             isGettingData = true;
 
@@ -231,64 +153,35 @@ public class DataCollectionService extends IntentService implements SensorEventL
         isGettingData = false;
     }
 
-    private void addGyroData(float[][] data){
-        this.gyroData = data;
-
-        if(accData != null) {
-            concatenateData();
-        }
-    }
-
-    private void addAccData(float[][] data){
-        this.accData = data;
-
-        if(gyroData != null){
-            concatenateData();
-        }
-    }
-
-    private void concatenateData(){
-
-        for(int i = 0; i < windowSize; i++){
-            inputData[i] = accData[0][i];
-        }
-        for (int i = 0; i < windowSize; i++){
-            inputData[i + 1 * windowSize] = accData[1][i];
-        }
-        for (int i = 0; i < windowSize; i++){
-            inputData[i + 2 * windowSize] = accData[2][i];
-        }
-        for (int i = 0; i < windowSize; i++){
-            inputData[i + 3 * windowSize] = gyroData[0][i];
-        }
-        for (int i = 0; i < windowSize; i++){
-            inputData[i + 4 * windowSize] = gyroData[1][i];
-        }
-        for (int i = 0; i < windowSize; i++){
-            inputData[i + 5 * windowSize] = gyroData[2][i];
-        }
-
-
-        Log.d("data", "inputData : " + inputData.toString() );
-        Log.d("data", "accData : " + accData.toString() );
-        Log.d("data", "gyroData : " + gyroData.toString() );
-
-        accData = new float[3][windowSize];
-        gyroData = new float[3][windowSize];
-
-        dataBuffer = "" + arrMax(doInference(inputData)[0]);
-
-        Log.d(TAG, "onSensorChanged: " + dataBuffer);
-        byte[] out = dataBuffer.getBytes(StandardCharsets.UTF_8);
-        btService.write(out);
-
+//    private void addAccData(float[][] data){
+//        accData = data;
+//        concatenateData();
+//    }
+//
+//    private void concatenateData(){
+//
+//        for(int i = 0; i < windowSize; i++){
+//            inputData[i] = accData[0][i];
+//        }
+//        for (int i = 0; i < windowSize; i++){
+//            inputData[i +  windowSize] = accData[1][i];
+//        }
+//        for (int i = 0; i < windowSize; i++){
+//            inputData[i + 2 * windowSize] = accData[2][i];
+//        }
+//
+//
+//        Log.d("data", "inputData : " + inputData.toString() );
+//        Log.d("data", "accData : " + accData.toString() );
+//
+//        accData = null;
+//
+//        float[] result = doInference(inputData)[0];
+//
 //        Log.d(TAG, "printArray: " + printArray(inputData).substring(0,3000));
-//        Log.d(TAG, "printArray: " + printArray(inputData).substring(3000,6000));
-//        Log.d(TAG, "printArray: " + printArray(inputData).substring(6000));
-
-//        Log.d(TAG, "input length: " + inputData.length);
-        Log.d(TAG, "probabilities: " + printArray(doInference(inputData)[0]));
-    }
+//        Log.d(TAG, "printArray: " + printArray(inputData).substring(3000));
+//        Log.d(TAG, "probabilities: " + printArray(result));
+//    }
 
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
@@ -299,111 +192,86 @@ public class DataCollectionService extends IntentService implements SensorEventL
             return;
         }
 
-        if(sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+        printArray(sensorEvent.values);
 
-            if (accTick >= windowSize) {
-                accTick = 0;
-                addAccData(deepClone(accSensorData));
-            } else {
-                accSensorData[0][accTick] = sensorEvent.values[0] / 9.80665f;
-                accSensorData[1][accTick] = sensorEvent.values[1] / 9.80665f;
-                accSensorData[2][accTick] = sensorEvent.values[2] / 9.80665f;
-
-                accTick++;
-            }
-        } if (sensorEvent.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
-
-            if (gyroTick >= windowSize) {
-                gyroTick = 0;
-                addGyroData(deepClone(gyroSensorData));
-            } else {
-                gyroSensorData[0][gyroTick] = sensorEvent.values[0];
-                gyroSensorData[1][gyroTick] = sensorEvent.values[1];
-                gyroSensorData[2][gyroTick] = sensorEvent.values[2];
-                gyroTick++;
-            }
-        }
-
-//        currTime = System.currentTimeMillis();
+//        if(sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
 //
-//        bufferQueue.add(sensorEvent.values[0]);
-//        bufferQueue.add(sensorEvent.values[1]);
-//        bufferQueue.add(sensorEvent.values[2]);
+//            if (accTick >= windowSize) {
+//                accTick = 0;
+//                addAccData(deepClone(accSensorData));
+//            } else {
+//                accSensorData[0][accTick] = sensorEvent.values[1] / 9.80665f;
+//                accSensorData[1][accTick] = sensorEvent.values[0] / 9.80665f;
+//                accSensorData[2][accTick] = sensorEvent.values[2] / 9.80665f;
 //
-//        tick++;
-//
-//        if(tick == 159){
-//            arrayAppender(inputData, bufferQueue);
+//                accTick++;
+//            }
 //        }
-
-
-
-
     }
 
-    public float[][] doInference(float[] input) {
-
-        float[][] output = new float[1][12];
-
-        try {
-            tflite.run(input, output);
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
-        return output;
-    }
+//    public float[][] doInference(float[] input) {
+//
+//        float[][] output = new float[1][12];
+//
+//        try {
+//            tflite.run(input, output);
+//        } catch (Exception e){
+//            e.printStackTrace();
+//        }
+//
+//        return output;
+//    }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
 
-    // Memory-map the model file from assets
-    private MappedByteBuffer loadModelFile() throws IOException {
-        // Open the model using an input stream, and memory map it to load
-        AssetFileDescriptor fileDescriptor = this.getAssets().openFd(modelFile);
-        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
-        FileChannel fileChannel = inputStream.getChannel();
-        long startOffset = fileDescriptor.getStartOffset();
-        long declaredLength = fileDescriptor.getDeclaredLength();
-        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
-    }
+//    // Memory-map the model file from assets
+//    private MappedByteBuffer loadModelFile() throws IOException {
+//        // Open the model using an input stream, and memory map it to load
+//        AssetFileDescriptor fileDescriptor = this.getAssets().openFd(modelFile);
+//        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+//        FileChannel fileChannel = inputStream.getChannel();
+//        long startOffset = fileDescriptor.getStartOffset();
+//        long declaredLength = fileDescriptor.getDeclaredLength();
+//        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+//    }
 
     public String printArray(float[] array) {
         String res = "";
 
         for (int i = 0; i < array.length; i++){
-//            Log.d(TAG, "printArray: " + array[i]);
+            Log.d(TAG, "printArray: " + array[i]);
             res += array[i] + (i == array.length-1 ? "" : ",");
 
         }
 
         return res;
     }
-
-    public int arrMax(float[] array) {
-        int highest = 0;
-
-        for (int i = 1; i < array.length; i++) {
-            if (array[i] > array[highest]){
-                highest = i;
-            }
-        }
-        Log.d(TAG, "arrMax: " + highest);
-        return highest;
-    }
-
-    public float[][] deepClone(float[][] array) {
-        if (array == null) {
-            return null;
-        }
-
-        final float[][] result = new float[array.length][];
-        for (int i = 0; i < array.length; i++) {
-            result[i] = Arrays.copyOf(array[i], array[i].length);
-        }
-        return result;
-    }
+//
+//    public int arrMax(float[] array) {
+//        int highest = 0;
+//
+//        for (int i = 1; i < array.length; i++) {
+//            if (array[i] > array[highest]){
+//                highest = i;
+//            }
+//        }
+//        Log.d(TAG, "arrMax: " + highest);
+//        return highest;
+//    }
+//
+//    public float[][] deepClone(float[][] array) {
+//        if (array == null) {
+//            return null;
+//        }
+//
+//        final float[][] result = new float[array.length][];
+//        for (int i = 0; i < array.length; i++) {
+//            result[i] = Arrays.copyOf(array[i], array[i].length);
+//        }
+//        return result;
+//    }
 
 }
